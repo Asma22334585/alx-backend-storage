59953ab4c38debe1c0e26a803973efe4cae56e82
Asma22334585 <guesmihadil0@gmail.com>
2024-07-14 00:34:42 +0000

    mycommit

--- 0x00-MySQL_Advanced/100-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -1,20 +1,14 @@
---creates a stored procedure 
-DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUser;
-DELIMITER $$
+-- creates a stored procedure
+DELIMITER //
 CREATE PROCEDURE ComputeAverageWeightedScoreForUser(IN user_id INT)
-BEGIN DECLARE weight_average_score FLOAT;
-    
-    SET weight_average_score = (
-	SELECT SUM(score * weight) / SUM(weight)
-	FROM users AS user
-	JOIN corrections AS corr 
-	ON user.id = corr.user_id
-	JOIN projects AS pro
-	On corr.project_id = pro.id
-	WHERE user.id = user_id
-    );
+BEGIN
     UPDATE users
-    SET average_score = weight_average_score
+    SET average_score = (
+        SELECT IF(SUM(score * weight) > 0, SUM(score * weight) / SUM(weight), 0)
+        FROM corrections
+        JOIN projects ON corrections.project_id = projects.id
+        WHERE corrections.user_id = user_id
+    )
     WHERE id = user_id;
-END $$
+END //
 DELIMITER ;
